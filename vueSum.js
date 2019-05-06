1b711678f9f437cb89c69cdb096c5e9e0e73c7e2
Lisa Burevestnik <lisapetrel1996@gmail.com>
2019-05-06 12:03:54 +0300

    added sum for only ceil number

--- vueSum.js
+++ vueSum.js
@@ -9,24 +9,30 @@ var app = new Vue({
         sum: function() {
             var num1 = document.getElementById('firstNumber').value;
             var num2 = document.getElementById('secondNumber').value;
-            var del_1 = ('' + Math.floor(Math.abs(+num1))).length + 1;
-            var del_2 = ('' + Math.floor(Math.abs(+num2))).length + 1;
-            if(+num1 < 0) {
-                del_1++;
+            var indexOfPoint_num1 = num1.indexOf('.');
+            var indexOfPoint_num2 = num2.indexOf('.');
+            if ((indexOfPoint_num1 != -1)) {
+                var len_num1 = (num1.slice(indexOfPoint_num1+1, num1.length)).length;
+                var point_num1 = '';
+            } else {
+                var len_num1 = 0;
+                var point_num1 = '.';
             }
-            if(+num2 < 0) {
-                del_2++;
+            if ((indexOfPoint_num2 != -1)) {
+                var len_num2 = (num2.slice(indexOfPoint_num2+1, num2.length)).length;
+                var point_num2 = '';
+            } else {
+                var len_num2 = 0;
+                var point_num2 = '.';
             }
-            var len_num1 = num1.length - del_1;
-            var len_num2 = num2.length - del_2;
             var del = len_num1;
             if(len_num1 >= len_num2) {
-                var float_2 = (num2).slice(0, len_num2 + del_2) + '0'.repeat(len_num1 - len_num2);
-                var float_1 = (num1).slice(0, len_num1 + del_1)
+                var float_2 = (num2).slice(0, num2.length) + point_num2 + '0'.repeat(len_num1 - len_num2)+'001';
+                var float_1 = (num1).slice(0, num1.length)
             } else  if(len_num2 > len_num1) {
                 del = len_num2;
-                var float_1 = (num1).slice(0, len_num1 + del_1) + '0'.repeat(len_num2 - len_num1);
-                var float_2 = (num2).slice(0, len_num2 + del_2)
+                var float_1 = (num1).slice(0, num1.length) + point_num1 + '0'.repeat(len_num2 - len_num1)+'001';
+                var float_2 = (num2).slice(0, num2.length)
             }
             this.number_1 = +float_1;
             console.log(this.number_1)
